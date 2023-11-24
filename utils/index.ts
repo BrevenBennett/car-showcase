b9ca7622d9a1e16ffbee6ae2d3340b69603c4a40
BrevenBennett <Breven.stars@gmail.com>
2023-11-24 13:52:56 -0600

    fixed searchParams bug

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "a3a449d050msh2be59d5d7ab372cp15bc52jsnbc83ce02ab01",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
