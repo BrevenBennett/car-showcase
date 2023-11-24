feef4295ebc46a3a9eb4d66d9ba4c1171b3e45fc
BrevenBennett <Breven.stars@gmail.com>
2023-11-24 13:17:03 -0600

    dynamic car images added via api

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
 import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -58,7 +59,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill
                         priority
@@ -69,7 +70,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className="flex gap-3">
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, '29')}
                           alt="car model"
                           fill
                           priority
@@ -78,7 +79,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, '33')}
                           alt="car model"
                           fill
                           priority
@@ -87,7 +88,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, '13')}
                           alt="car model"
                           fill
                           priority
@@ -104,9 +105,9 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
 
                     <div className="mt-3 flex flex-wrap gap-4">
                       {Object.entries(car).map(([key, value]) => (
-                        <div className="flex justify-bewteen gap-5 w-full text-right" key={key}>
+                        <div className="flex justify-between gap-5 w-full text-right" key={key}>
                           <h4 className="text-gray capitalize">{key.split("_").join(" ")}</h4>
-                          <p className="text-black-100 font-semibold">{value}</p>
+                          <p className="text-black-100 font-semibold capitalize">{value}</p>
                         </div>
                       ))}
                     </div>
